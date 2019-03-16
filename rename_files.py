7723fc337d895b963f28100bfeaa81a350bfafab
DREWSKYnolen <anolen3@radford.edu>
2019-03-16 16:59:47 -0400

    gets working directory and stores it for future use

--- rename_files.py
+++ rename_files.py
@@ -3,17 +3,18 @@ import os
 def rename_files():
     #(1) get file names from a folder
     #Dynamically get location based on this file's location
-    #file_list = os.listdir(r"C:\Users\dnolen\Documents\Udacity Classes\course-python\file-rename\prank\prank")
-    print(file_list)
+    file_path = os.getcwd()
 
-    saved_path = os.getcwd()
-    
-    #os.chdir(r"C:\Users\dnolen\Documents\Udacity Classes\course-python\file-rename\prank\prank")
+
     
     #(2) for each file, rename filename
-    translation_table = str.maketrans("0123456789", "          ", "0123456789")
-    for file_name in file_list:
-        os.rename(file_name, file_name.translate(translation_table))
-    os.chdir(saved_path)
+    #header of file with 4A464946 is JFIF in ascii (which is a jpg/png file)
+
+
+
+    # translation_table = str.maketrans("0123456789", "          ", "0123456789")
+    # for file_name in file_list:
+    #     os.rename(file_name, file_name.translate(translation_table))
+    # os.chdir(saved_path)
 
 rename_files()
