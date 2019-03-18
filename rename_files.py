5bc63c8db4e7c08427cc240329b1f23caa201232
DREWSKYnolen <anolen3@radford.edu>
2019-03-18 16:22:18 -0400

    Close file after reading and before renaming. Expand to include .jpg, .gif, .png, .mp4, .pdf

--- rename_files.py
+++ rename_files.py
@@ -12,13 +12,42 @@ def rename_files():
     #(2) for each file, rename filename
 
     #filename = 'f_019916'
+    jfif_match = "<re.Match object; span=(0, 16), match=b'\xff\xd8\xff\xe0\x00\x10JFIF\x00\x01\x02\x00\x00>"
    
 
     for filename in os.listdir(file_path):
+        print(filename)
+        ext = ""
         with open(filename, 'rb') as f:
             content = re.search(b'\xFF\xD8\xFF\xE0\x00\x10JFIF\x00\x01\x02\x00\x00\x01', f.read())
-            if content is not "None":
-                os.rename(filename,filename + ".jpg")
+            #if this is a jpg
+            if content is not None:
+                ext = ".jpg"
+        with open(filename, 'rb') as f:
+            content = re.search(b'\x89\x50\x4E\x47', f.read())
+            #if this is a png
+            if content is not None:
+                ext = ".png"
+        with open(filename, 'rb') as f:
+            content = re.search(b'\x47\x49\x46', f.read())
+            #if this is a gif
+            if content is not None:
+                ext = ".gif"
+        with open(filename, 'rb') as f:
+            content = re.search(b'\x69\x73\x6F\x6D', f.read())
+            #if this is an mp4
+            if content is not None:
+                ext = ".mp4"
+        with open(filename, 'rb') as f:
+            content = re.search(b'\x25\x50\x44\x46', f.read())
+            #if this is a PDF
+            if content is not None:
+                ext = ".pdf"
+            
+            f.close()
+            print(ext)
+            os.rename(filename,filename + ext)
+                
     
     
 
