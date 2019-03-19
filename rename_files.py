c3496d8ecb7f4f2b945ceab0dec71be46fbdc444
DREWSKYnolen <anolen3@radford.edu>
2019-03-19 12:09:56 -0400

    change header search size to 48 hex values

--- rename_files.py
+++ rename_files.py
@@ -15,27 +15,27 @@ def rename_files():
         print(filename)
         ext = ""
         with open(filename, 'rb') as f:
-            content = re.search(b'\xFF\xD8\xFF\xE0\x00\x10JFIF\x00\x01\x02\x00\x00\x01', f.read()[0:20])
+            content = re.search(b'\xFF\xD8\xFF\xE0\x00\x10JFIF\x00\x01\x02\x00\x00\x01', f.read()[0:48])
             #if this is a jpg
             if content is not None:
                 ext = ".jpg"
         with open(filename, 'rb') as f:
-            content = re.search(b'\x89\x50\x4E\x47', f.read()[0:20])
+            content = re.search(b'\x89\x50\x4E\x47', f.read()[0:48])
             #if this is a png
             if content is not None:
                 ext = ".png"
         with open(filename, 'rb') as f:
-            content = re.search(b'\x47\x49\x46', f.read()[0:20])
+            content = re.search(b'\x47\x49\x46', f.read()[0:48])
             #if this is a gif
             if content is not None:
                 ext = ".gif"
         with open(filename, 'rb') as f:
-            content = re.search(b'\x69\x73\x6F\x6D', f.read()[0:20])
+            content = re.search(b'\x69\x73\x6F\x6D', f.read()[0:48])
             #if this is an mp4
             if content is not None:
                 ext = ".mp4"
         with open(filename, 'rb') as f:
-            content = re.search(b'\x25\x50\x44\x46', f.read()[0:20])
+            content = re.search(b'\x25\x50\x44\x46', f.read()[0:48])
             #if this is a PDF
             if content is not None:
                 ext = ".pdf"
